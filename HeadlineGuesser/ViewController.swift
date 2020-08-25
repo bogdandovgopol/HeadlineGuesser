e2296357270742f4ce6137fe077b21473b4ab34b
Bogdan Dovgopol <bogdan@dovgopol.dev>
2020-08-25 10:05:06 +1000

    Initial Commit

---
+++ HeadlineGuesser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeadlineGuesser
+//
+//  Created by Bogdan on 25/8/20.
+//  Copyright © 2020 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
